e4f79bf0b284322605f4223098a3c4aa4f3b70f6
solankiayu <solankiayush43@gmail.com>
2022-11-22 20:43:13 +0530

    update code

--- deliveree_go/deliveree_go_users/urls.py
+++ deliveree_go/deliveree_go_users/urls.py
@@ -3,10 +3,13 @@ from deliveree_go_users import viewsets
 
 urlpatterns = [
     path('addressAfterLogin',viewsets.addressAfterLogin.as_view()),
-    path('shopData',viewsets.shopData.as_view()),
     path('home_pageApi',viewsets.home_pageApi.as_view()),
     path('itemsListData',viewsets.itemsListData.as_view()),
     path('shopsListData',viewsets.shopsListData.as_view()),
     path('cart_list',viewsets.cart_list.as_view()),
+
+
+    path('shopData',viewsets.shopData.as_view()),
+    path('ItemData',viewsets.ItemData.as_view()),
     
 ]
